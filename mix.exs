cde767168f6298d54092f6bac79559568164526e
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2023-01-11 14:29:33 +0100

    Change license to the correct SPDX identifier.

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Membrane.Telemetry.TimescaleDB.Mixfile do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
